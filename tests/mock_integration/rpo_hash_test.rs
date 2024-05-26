b9672c5488a64fdfd39c2149f1a4724a4e0b9791
riemann <aleqvids@gmail.com>
2024-05-25 21:43:41 -0400

    recipient test updates

--- tests/mock_integration/rpo_hash_test.rs
+++ tests/mock_integration/rpo_hash_test.rs
@@ -1,9 +1,29 @@
-use miden_vm::{prove, verify, Assembler, DefaultHost, ProvingOptions, StackInputs};
 use miden_objects::{
-  notes::
-      NoteInputs,
-  Felt, Hasher, Digest
+    assembly::{AssemblyContext, ModuleAst, ProgramAst},
+    notes::{NoteInputs, NoteRecipient, NoteScript},
+    vm::CodeBlock,
+    Digest, Felt, Hasher, NoteError,
 };
+use miden_vm::{prove, verify, Assembler, DefaultHost, ProvingOptions, StackInputs};
+
+pub fn new_note_script(
+    code: ProgramAst,
+    assembler: &Assembler,
+) -> Result<(NoteScript, CodeBlock), NoteError> {
+    // Compile the code in the context with phantom calls enabled
+    let code_block = assembler
+        .compile_in_context(
+            &code,
+            &mut AssemblyContext::for_program(Some(&code)).with_phantom_calls(true),
+        )
+        .map_err(NoteError::ScriptCompilationError)?;
+
+    // Use the from_parts method to create a NoteScript instance
+    let note_script = NoteScript::from_parts(code, code_block.hash());
+
+    Ok((note_script, code_block))
+}
+
 #[test]
 fn test_rpo_hash() {
     // Instantiate the assembler
@@ -26,8 +46,7 @@ fn test_rpo_hash() {
     let (outputs, proof) = prove(&program, stack_inputs, host, ProvingOptions::default())
         .expect("Failed to execute the program and generate a proof");
 
-    println!("Stack output:");
-    println!("{:?}", outputs.stack());
+    println!("Stack Output: {:?}", outputs.stack());
 
     let inputs = NoteInputs::new(vec![Felt::new(2)]).unwrap();
 
@@ -37,6 +56,24 @@ fn test_rpo_hash() {
     let serial_num_hash = Hasher::merge(&[serial_num.into(), Digest::default()]);
 
     println!("Serial Number Hash: {:?}", serial_num_hash);
+
+    let note_script_code = include_str!("../../src/hash/basic_note.masm");
+    let note_script = new_note_script(ProgramAst::parse(note_script_code).unwrap(), &assembler)
+        .unwrap()
+        .0;
+
+    let note_script_hash: Digest = note_script.hash();
+
+    println!("Note Script Hash: {:?}", note_script_hash);
+
+    let serial_script_hash = Hasher::merge(&[serial_num_hash, note_script_hash]);
+
+    let recipient_1 = Hasher::merge(&[serial_script_hash, inputs.commitment()]);
+
+    println!("recipient_1: {:?}", recipient_1);
+    let recipient = NoteRecipient::new(serial_num, note_script, inputs);
+
+    print!("Recipient: {:?}", recipient.digest());
 
     verify(program.into(), cloned_inputs, outputs, proof).unwrap();
     println!("Program run successfully");
